69830edc79970a5e690291fc0628b5802fa2efae
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2024-08-07 13:57:46 -0400

    update layout

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/threejs-webflow-periodictable/', // Replace with your repository name
+  build: {
+    outDir: 'dist', // Output directory
+  }
+});
